3e6c3940731b795bb285b20fff9ad4354527a8d9
Rashmi Maharjan <rashmimaharjan1@gmail.com>
2024-09-06 22:54:33 +0545

    reporter reimplemented

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,15 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://www.saucedemo.com/v1/',
 
-    reporter: 'cypress-mochawesome-reporter',
+    reporter: 'mochawesome',
+    reporterOptions: {
+      "charts":true,
+      "overwrite": false,
+     "html":false,
+      "json": true,
+      "reporterDir": "cypress/reports/"
+
+    },
 
     setupNodeEvents(on, config) {
       // implement node event listeners here
